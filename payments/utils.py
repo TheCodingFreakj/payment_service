f610480349310e8b645c83418a1efcd9c2f79076
pallavy57 <priyadarshinipallavi18@gmail.com>
2024-07-21 00:33:11 +0530

    refactor for logs

--- payments/utils.py
+++ payments/utils.py
@@ -3,7 +3,7 @@ import logging
 import time
 from .logsProducer import send_log, get_user_location
 # Configure the logging
-logging.basicConfig(level=logging.info)
+logging.basicConfig(level=logging.debug)
 from .loggin_config import logger
 
 class CircuitBreakerError(Exception):
@@ -17,51 +17,51 @@ class CircuitBreaker:
         self.failures = 0
         self.last_failure_time = 0
         self.state = 'CLOSED'
-        logger.info("Initialized CircuitBreaker with failure_threshold=%d, recovery_time=%d", failure_threshold, recovery_time)
+        logger.debug("Initialized CircuitBreaker with failure_threshold=%d, recovery_time=%d", failure_threshold, recovery_time)
         
     def call(self, func, *args, **kwargs):
-        logger.info("CircuitBreaker call: state=%s, failures=%d", self.state, self.failures)
+        logger.debug("CircuitBreaker call: state=%s, failures=%d", self.state, self.failures)
         if self.state == 'OPEN':
             if time.time() - self.last_failure_time > self.recovery_time:
                 self.state = 'HALF-OPEN'
-                logger.info("Transitioning to HALF-OPEN state")
+                logger.debug("Transitioning to HALF-OPEN state")
             else:
                 logger.warning("Circuit breaker is open. Cannot call function.")
-                raise Exception('Circuit breaker is open')
+                return CircuitBreakerError('Circuit breaker is open')
 
         try:
             result = func(*args, **kwargs)
-            logger.info(f"Function call successful for args, {args}")
-            logger.info(f"Function call successful for kwargs, {kwargs}")
+            logger.debug(f"Function call successful for args: {args}")
+            logger.debug(f"Function call successful for kwargs: {kwargs}")
             self.reset()
-            logger.info(f"Function call successful, {result}")
+            logger.debug(f"Function call successful, result: {result}")
             transaction_data = {
-                    'transaction_id':"transaction_id",
-                    'user_id': 1,
-                    'payment_method': "POST",
-                    'status': 'processing',
-                    'initiated_at': datetime.datetime.now().isoformat(),
-                    'location': get_user_location("ip_address")
-                }
-                # Log the transaction initiation
+                'transaction_id': "transaction_id",
+                'user_id': 1,
+                'payment_method': "POST",
+                'status': 'processing',
+                'initiated_at': datetime.datetime.now().isoformat(),
+                'location': get_user_location("ip_address")
+            }
+            # Log the transaction initiation
             send_log({'type': 'transaction', 'data': transaction_data})
-            logger.info("Function call successful, circuit breaker state reset to CLOSED")
+            logger.debug("Function call successful, circuit breaker state reset to CLOSED")
             return result
         except Exception as e:
             self.failures += 1
             transaction_data = {
-                    'transaction_id':"transaction_id",
-                    'user_id': 1,
-                    'payment_method': "POST",
-                    'status': 'failed',
-                    'initiated_at': datetime.datetime.now().isoformat(),
-                    'location': get_user_location("ip_address")
-                }
-                # Log the transaction initiation
+                'transaction_id': "transaction_id",
+                'user_id': 1,
+                'payment_method': "POST",
+                'status': 'failed',
+                'initiated_at': datetime.datetime.now().isoformat(),
+                'location': get_user_location("ip_address")
+            }
+            # Log the transaction failure
             send_log({'type': 'transaction', 'data': transaction_data})
             self.last_failure_time = time.time()
             logger.error("Function call failed: %s", e)
-            logger.info("Failure count increased to %d", self.failures)
+            logger.debug("Failure count increased to %d", self.failures)
             if self.failures >= self.failure_threshold:
                 self.state = 'OPEN'
                 logger.warning("Failure threshold reached. Transitioning to OPEN state")
@@ -70,4 +70,4 @@ class CircuitBreaker:
     def reset(self):
         self.failures = 0
         self.state = 'CLOSED'
-        logger.info("CircuitBreaker reset: state=CLOSED, failures=0")
+        logger.debug("CircuitBreaker reset: state=CLOSED, failures=0")
