8da450dad81a125d1d0c33b91e8042de194924c5
pallavy57 <priyadarshinipallavi18@gmail.com>
2024-07-25 00:15:55 +0530

    kafka ssl

--- payments/kafka_producer.py
+++ payments/kafka_producer.py
@@ -13,13 +13,12 @@ class KafkaProducerService:
             bootstrap_servers=settings.KAFKA_BROKER_URLS,
             security_protocol='SSL',
             ssl_context=context,
-            value_serializer=lambda v: json.dumps(v).encode('utf-8')
-            # sasl_mechanism = 'SCRAM-SHA-256',
-            # sasl_plain_username=settings.KAFKA_USERNAME,
-            # sasl_plain_password=settings.KAFKA_PASSWORD,
-            # security_protocol="SASL_SSL",
-            # ssl_cafile=settings.KAFKA_CA_CERT,
-            #value_serializer=lambda v: json.dumps(v).encode('utf-8')
+            value_serializer=lambda v: json.dumps(v).encode('utf-8'),
+            acks='all',
+            retries=5,
+            enable_idempotence=True,
+            request_timeout_ms=30000,  # Request timeout
+            delivery_timeout_ms=30000  # Delivery timeout
         )
     def send_message(self, topic, value):
         self.producer.send(topic, value)
